2cf589881567e9b4e39377d8b08c19c4c2576f22
shunanovke <shunan05@gmail.com>
2023-09-19 22:34:16 +0500

    Task1

---
+++ Tyuiu.ShunanovKE.Sprint1.Task1.V4.Test/Tyuiu.ShunanovKE.Sprint1.Task1.V4.Test.cpp
@@ -0,0 +1,24 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Tyuiu.ShunanovKE.Sprint1.Task1.V4.Lib/Tyuiu.ShunanovKE.Sprint1.Task1.V4.Lib.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TyuiuShunanovKESprint1Task1V4Test
+{
+	TEST_CLASS(TyuiuShunanovKESprint1Task1V4Test)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			ISprint1Task1* d = new Service();
+			int a = 137;
+			bool ans;
+
+			ans = d->Logic(a);
+
+			Assert::AreEqual(ans, true);
+		}
+	};
+}
---
+++ Tyuiu.ShunanovKE.Sprint1.Task1.V4/Tyuiu.ShunanovKE.Sprint1.Task1.V4.cpp
@@ -0,0 +1,18 @@
+﻿// Tyuiu.ShunanovKE.Sprint1.Task1.V4.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "../Tyuiu.ShunanovKE.Sprint1.Task1.V4.Lib/Tyuiu.ShunanovKE.Sprint1.Task1.V4.Lib.cpp"
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+    ISprint1Task1* d = new Service();
+    std::cout << "Введите целое число: ";
+    int a;
+    std::cin >> a;
+    if (d->Logic(a))
+        std::cout << "Да, число заканчивается цифрой 7";
+    if (d->Logic(a) == false)
+        std::cout << "Нет, число не заканчивается цифрой 7";
+}
