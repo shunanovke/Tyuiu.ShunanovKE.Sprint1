c92067472591c8c99ad93c73f8f96f9db52117cd
shunanovke <shunan05@gmail.com>
2023-09-19 12:36:41 +0500

    Add project files.

---
+++ Tyuiu.ShunanovKE.Sprint1.Task0.V4.Test/Tyuiu.ShunanovKE.Sprint1.Task0.V4.Test.cpp
@@ -0,0 +1,28 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Tyuiu.ShunanovKE.Sprint1.Task0.V4.Lib/Tyuiu.ShunanovKE.Sprint1.Task0.V4.Lib.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest0
+{
+	TEST_CLASS(UnitTest0)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			//Init
+			ISprint1Task0V00* c = new Service();
+			int a = 6;
+			int b = 4;
+			int ans;
+
+			//run
+			ans = c->Calculate(a, b);
+
+			Assert::AreEqual(ans, 24);
+
+		}
+	};
+}
---
+++ Tyuiu.ShunanovKE.Sprint1.Task0.V4/Tyuiu.ShunanovKE.Sprint1.Task0.V4.cpp
@@ -0,0 +1,17 @@
+﻿// Tyuiu.ShunanovKE.Sprint1.Task0.V4.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "../Tyuiu.ShunanovKE.Sprint1.Task0.V4.Lib/Tyuiu.ShunanovKE.Sprint1.Task0.V4.Lib.cpp"
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+    int a, b;
+    ISprint1Task0V00* c = new Service();
+    std::cout << "Введите первую сторону прямоугольника: ";
+    std::cin >> a;
+    std::cout << "Введите вторую сторону прямоугольника: ";
+    std::cin >> b;
+    std::cout << "Площадь прямоугольника = " << c->Calculate(a, b);
+}
