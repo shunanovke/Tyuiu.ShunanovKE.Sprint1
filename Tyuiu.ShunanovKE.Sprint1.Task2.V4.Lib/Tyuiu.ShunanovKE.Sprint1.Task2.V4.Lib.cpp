ad846a231e9eb4cb1c90ce8f6dae1d4a00d988d8
shunanovke <shunan05@gmail.com>
2023-09-20 12:24:45 +0500

    Task2

---
+++ Tyuiu.ShunanovKE.Sprint1.Task2.V4.Lib/Tyuiu.ShunanovKE.Sprint1.Task2.V4.Lib.cpp
@@ -0,0 +1,15 @@
+﻿// Tyuiu.ShunanovKE.Sprint1.Task2.V4.Lib.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "../../Tyuiu.Cours.cpp/Tyuiu.Cours.cpp.cpp"
+
+// TODO: This is an example of a library function
+class Service : public ISprint1Task2
+{
+	virtual bool LogicLong(int a) override
+	{
+		return (a % 10) % 3 == 0;
+	}
+};
