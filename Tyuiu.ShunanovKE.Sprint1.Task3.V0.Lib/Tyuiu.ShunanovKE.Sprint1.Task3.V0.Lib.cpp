6f45294286a9cfcb9b09532b82617b450f84ac86
shunanovke <shunan05@gmail.com>
2023-09-21 19:28:35 +0500

    Task3

---
+++ Tyuiu.ShunanovKE.Sprint1.Task3.V0.Lib/Tyuiu.ShunanovKE.Sprint1.Task3.V0.Lib.cpp
@@ -0,0 +1,15 @@
+﻿// Tyuiu.ShunanovKE.Sprint1.Task3.V0.Lib.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "../../Tyuiu.Cours.cpp/Tyuiu.Cours.cpp.cpp"
+
+// TODO: This is an example of a library function
+class Service : public ISprint1Task2
+{
+	virtual bool LogicLong(int a) override
+	{
+		return (a % 4 == 0) && (a != 1000);
+	}
+};
---
+++ Tyuiu.ShunanovKE.Sprint1.Task3.V0.Test/Tyuiu.ShunanovKE.Sprint1.Task3.V0.Test.cpp
@@ -0,0 +1,26 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Tyuiu.ShunanovKE.Sprint1.Task3.V0.Lib/Tyuiu.ShunanovKE.Sprint1.Task3.V0.Lib.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest3
+{
+	TEST_CLASS(UnitTest3)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			//init
+			ISprint1Task2* d = new Service();
+			int a = 2048;
+			bool ans;
+
+			//run
+			ans = d->LogicLong(a);
+
+			Assert::AreEqual(ans, true);
+		}
+	};
+}
