72b6ad68aa6faeaf1bdf1e2696688cd31a8d4ae6
shunanovke <shunan05@gmail.com>
2023-10-03 22:21:45 +0500

    Task0

---
+++ Tyuiu.ShunanovKE.Sprint1.Task0.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ShunanovKE.Sprint1.Task0.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Tyuiu.ShunanovKE.Sprint1.Task0.V0.Test")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cfcdefe3-813a-47f5-8f49-a547b199c932")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
