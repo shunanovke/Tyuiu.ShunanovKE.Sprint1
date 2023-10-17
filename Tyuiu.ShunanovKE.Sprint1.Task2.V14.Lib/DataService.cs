57811527481c0788a266228ff521a06bd193c118
shunanovke <shunan05@gmail.com>
2023-10-17 19:21:22 +0500

    Task2

--- Tyuiu.ShunanovKE.Sprint1.Task2.V14.Lib/DataService.cs
+++ Tyuiu.ShunanovKE.Sprint1.Task2.V14.Lib/DataService.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using tyuiu.cources.programming.interfaces.Sprint1;
 
 namespace Tyuiu.ShunanovKE.Sprint1.Task2.V14.Lib
