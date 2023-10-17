b957ea60a2d2deb0e9f192f986ca11be37bf3681
shunanovke <shunan05@gmail.com>
2023-10-17 19:20:54 +0500

    Task1

--- Tyuiu.ShunanovKE.Sprint1.Task1.V26.Lib/DataService.cs
+++ Tyuiu.ShunanovKE.Sprint1.Task1.V26.Lib/DataService.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using tyuiu.cources.programming.interfaces.Sprint1;
 
 namespace Tyuiu.ShunanovKE.Sprint1.Task1.V26.Lib
